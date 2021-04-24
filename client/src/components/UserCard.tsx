1eb1d206d0177fe2a6c9f6370fef60e65f760b68
Rafael Fontoura <rafael.fontoura@gmail.com>
2021-04-24 13:56:06 -0300

    Change card layout to full-width squared picture

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -20,9 +20,10 @@ type AvatarOrInitialsType = {
 const AvatarOrInitials = ({ user, avatarStyle }: AvatarOrInitialsType) => {
     const { avatarUrl, name, login } = user;
     if (avatarUrl) {
+        const pictureName = `${user.name || user.login} picture`;
         return (
-            <div>
-                <Avatar variant="circular" src={user.avatarUrl} className={avatarStyle} />
+            <div className={avatarStyle}>
+                <img src={user.avatarUrl} className={avatarStyle} alt={pictureName} />
             </div>
         );
     }
@@ -45,7 +46,7 @@ const GitHubStar = ({ user }: { user: User }) => {
 };
 
 const useStyles = makeStyles((theme) => {
-    const cardWidth = theme.spacing(20);
+    const cardWidth = theme.spacing(32);
     return {
         icon: {
             root: {
@@ -55,17 +56,28 @@ const useStyles = makeStyles((theme) => {
             alignItems: 'center',
             justifyContent: 'center',
             marginRight: theme.spacing(0.5),
+            fontSize: '1.3rem',
         },
         container: {
             margin: theme.spacing(1),
-            padding: theme.spacing(2),
-            width: cardWidth + theme.spacing(12),
+            width: cardWidth,
             display: 'flex',
             flexDirection: 'column',
+        },
+        contentContainer: {
+            padding: `${theme.spacing(1)}px ${theme.spacing(2)}px ${theme.spacing(2)}px`,
+            display: 'flex',
+            flexDirection: 'column',
+            fontSize: '0.85rem',
+            '&> div': {
+                marginTop: theme.spacing(0.5),
+            },
         },
         avatar: {
             height: cardWidth,
             width: cardWidth,
+            borderTopRightRadius: theme.spacing(0.5),
+            borderTopLeftRadius: theme.spacing(0.5),
         },
         avatarContainer: {
             display: 'flex',
@@ -74,12 +86,14 @@ const useStyles = makeStyles((theme) => {
         flexCenter: {
             display: 'flex',
             alignItems: 'center',
-            marginTop: theme.spacing(1),
         },
         name: {
-            marginTop: theme.spacing(1),
             fontWeight: 700,
             fontSize: '1.2rem',
+        },
+        bio: {
+            marginTop: theme.spacing(0.5),
+            textOverflow: 'ellipsis',
         },
         login: {
             marginTop: '0.2rem',
@@ -87,7 +101,6 @@ const useStyles = makeStyles((theme) => {
             color: '#888888',
         },
         followersLine: {
-            marginTop: '0.5rem',
             display: 'flex',
             alignItems: 'center',
             whiteSpace: 'nowrap',
@@ -109,55 +122,57 @@ const UserCard: FunctionComponent<UserCardType> = ({ user }: UserCardType) => {
     const styles = useStyles();
     return (
         <Paper className={styles.container}>
-            <div className={styles.avatarContainer}>
-                <AvatarOrInitials user={user} avatarStyle={styles.avatar} />
-            </div>
-            <div className={styles.flexCenter}>
-                {user.name && (
-                    <div className={styles.flexCenter}>
-                        <div
-                            title="This person is a GitHub star!"
-                            style={{ lineHeight: '0.5rem' }}
-                            className={styles.flexCenter}
-                        >
-                            <a rel="noreferrer" href="https://stars.github.com/profiles/" target="_blank">
-                                <GitHubStar user={user} />
-                            </a>
+            <AvatarOrInitials user={user} avatarStyle={styles.avatar} />
+            <div className={styles.contentContainer}>
+                <div className={styles.flexCenter}>
+                    {user.name && (
+                        <div className={styles.flexCenter}>
+                            <div style={{ lineHeight: '0.5rem' }} className={styles.flexCenter}>
+                                <a
+                                    rel="noreferrer"
+                                    href="https://stars.github.com/profiles/"
+                                    target="_blank"
+                                    title="This person is a GitHub star!"
+                                    style={{ marginLeft: '-8px' }}
+                                >
+                                    <GitHubStar user={user} />
+                                </a>
+                            </div>
+                            <div className={styles.name}>{user.name}</div>
                         </div>
-                        <div className={styles.name}>{user.name}</div>
+                    )}
+                </div>
+                <div className={styles.login}>{user.login}aa</div>
+                {user.bio && <div className={styles.bio}>{user.bio}</div>}
+                {user.company && (
+                    <div className={styles.flexCenter}>
+                        <div>
+                            <BusinessIcon color="primary" className={styles.icon} />
+                        </div>
+                        <div>{user.company}</div>
                     </div>
                 )}
-            </div>
-            <div className={styles.login}>{user.login}aa</div>
-            {user.bio && <div>{user.bio}</div>}
-            {user.company && (
-                <div className={styles.flexCenter}>
-                    <div className={styles.icon}>
-                        <BusinessIcon color="primary" />
+                {user.location && (
+                    <div className={styles.flexCenter}>
+                        <div className={styles.icon}>
+                            <LocationOnOutlinedIcon color="primary" />
+                        </div>
+                        <div>{user.location}</div>
                     </div>
-                    <div>{user.company}</div>
-                </div>
-            )}
-            {user.location && (
-                <div className={styles.flexCenter}>
+                )}
+                <div className={styles.followersLine}>
                     <div className={styles.icon}>
-                        <LocationOnOutlinedIcon color="primary" />
+                        <PeopleAltOutlinedIcon className={styles.icon} />
                     </div>
-                    <div>{user.location}</div>
+                    {user.followers} followers · {user.following} following
                 </div>
-            )}
-            <div className={styles.followersLine}>
-                <div className={styles.icon}>
-                    <PeopleAltOutlinedIcon className={styles.icon} />
+                <div className={styles.flexCenter}>
+                    Repositories: {user.repositories} ·{' '}
+                    <div>
+                        <StarRateRoundedIcon className={styles.icon} />
+                    </div>
+                    <span title={`${user.starredRepositories} starred repositories`}>{user.starredRepositories}</span>
                 </div>
-                {user.followers} followers · {user.following} following ·{' '}
-            </div>
-            <div className={styles.flexCenter}>
-                Repositories: {user.repositories} ·{' '}
-                <div>
-                    <StarRateRoundedIcon />
-                </div>
-                <span title={`${user.starredRepositories} starred repositories`}>{user.starredRepositories}</span>
             </div>
         </Paper>
     );
